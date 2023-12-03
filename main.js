5a520cba86297bf08337fced090f50e3a5ecc925
Emil Högberg <emil.hogberg@gmail.com>
2023-12-03 15:27:49 +0100

    fixed error message in console, moved JS code that fetches JSON data to a new file

--- main.js
+++ main.js
@@ -37,46 +37,6 @@ if (isDarkModeEnabled) {
     });
     darkModeToggle.checked = true;
 }
-
-// Hämta data från JSON filen
-async function getData() {
-    try {
-        // Hämta JSON filen
-        let response = await fetch("../cv.json");
-
-        // Kolla om hämtningen var OK 200
-        if (!response.ok) {
-            console.log("HTTP ERROR:" + response.status);
-        }
-
-        // Parscha texten
-        let myData = await response.json();
-
-        // Komma åt arrayen i JSON filen
-        let experiences = myData.experiences;
-
-        // Skapa HTML element för texten
-        let xp = "";
-        experiences.forEach((experience, index) => {
-            //Skapa klasser på varje p
-            let companyClass = `company-${index}`;
-            let yearClass = `year-${index}`;
-            let titleClass = `title-${index}`;
-
-            xp += `<div>
-            <p class="${titleClass}">${experience.title}</p>
-            <p class="${yearClass}">${experience.year}</p>
-            <p class="${companyClass}">${experience.company}</p>
-                </div>`;
-        })
-        document.getElementById("myData").innerHTML = xp;
-    } catch (error) {
-        console.error('Error fetching data:', error.message);
-    }
-}
-
-getData();
-
 // Modal Window Script - Denna kod är tagen från en annan sida
 
 function openModal(url) {
